57cf2a0be891c618cc78abcfa7b012a01d493447
Rajasegar <rajasegar.c@gmail.com>
2021-08-19 15:56:46 +0530

    chore: first commit class based components version

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,97 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.jsx';
+import AddTodo from './AddTodo.jsx';
+import TodoList from './TodoList.jsx';
+import TodoFilter from './TodoFilter.jsx';
+import StateContext from './StateContext';
+import { fetchAPITodos, generateID } from './api';
 
-function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [], filteredTodos: [], filter: 'all' };
+
+    this.fetchTodos = this.fetchTodos.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.toggleTodo = this.toggleTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.filterTodos = this.filterTodos.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTodos();
+  }
+
+  fetchTodos() {
+    fetchAPITodos().then((todos) => {
+      this.setState({ todos });
+      this.filterTodos();
+    });
+  }
+
+  addTodo(title) {
+    const { todos } = this.state;
+    const newTodo = { id: generateID(), title, completed: false };
+    this.setState({ todos: [newTodo, ...todos] });
+    this.filterTodos();
+  }
+
+  toggleTodo(id) {
+    const { todos } = this.state;
+    const newTodos = todos.map((t) => {
+      if (t.id === id) {
+        return { ...t, completed: !t.completed };
+      }
+      return t;
+    }, []);
+
+    this.setState({ todos: newTodos });
+    this.filterTodos();
+  }
+
+  filterTodos(filterArg) {
+    this.setState(({ todos, filter }) => ({
+      filter: filterArg || filter,
+      filteredTodos: this.applyFilter(todos, filterArg || filter),
+    }));
+  }
+
+  removeTodo(id) {
+    const { todos } = this.state;
+    const newTodos = todos.filter((t) => t.id !== id);
+    this.setState({ todos: newTodos });
+    this.filterTodos();
+  }
+
+  applyFilter(todos, filter) {
+    switch (filter) {
+      case 'active':
+        return todos.filter((t) => t.completed === false);
+
+      case 'completed':
+        return todos.filter((t) => t.completed === true);
+
+      default:
+      case 'all':
+        return todos;
+    }
+  }
+
+  render() {
+    const { filter, filteredTodos } = this.state;
+
+    return (
+      <StateContext.Provider value={filteredTodos}>
+        <div style={{ width: 400 }}>
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <hr />
+          <TodoList toggleTodo={this.toggleTodo} removeTodo={this.removeTodo} />
+          <hr />
+          <TodoFilter filter={filter} filterTodos={this.filterTodos} />
+        </div>
+      </StateContext.Provider>
+    );
+  }
 }
-
-export default App;
---
+++ src/TodoItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleToggle() {
+    const { toggleTodo, id } = this.props;
+    toggleTodo(id);
+  }
+
+  handleRemove() {
+    const { removeTodo, id } = this.props;
+    removeTodo(id);
+  }
+
+  render() {
+    const { title, completed } = this.props;
+    return (
+      <div style={{ width: 400, height: 25 }}>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={this.handleToggle}
+        />
+        {title}
+        <button style={{ float: 'right' }} onClick={this.handleRemove}>
+          x
+        </button>
+      </div>
+    );
+  }
+}
